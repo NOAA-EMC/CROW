4b34c12959b77734f9d946fb7319783e80d1b175
samuel.trahan <Samuel.Trahan@noaa.gov>
2017-09-12 11:43:17 +0000

    setup_workflow.py writes the Rocoot xml file

--- examples/real-workflow/workflow/setup_workflow.py
+++ examples/real-workflow/workflow/setup_workflow.py
@@ -1,17 +1,27 @@
 #! /usr/bin/env python3.6
 
-import sys, os
+import sys, os, logging
 import crow.config
 import crow.metascheduler
 
+logging.basicConfig(stream=sys.stderr,level=logging.INFO,
+   format='%(module)s:%(lineno)d: %(levelname)8s: %(message)s')
+logger=logging.getLogger('setup_workflow')
+
 run_dir=sys.argv[1]
+logger.info(f'Run directory: {run_dir}')
 config_yaml=os.path.join(run_dir,'config.yaml')
+logger.info(f'Config file: {config_yaml}')
 conf=crow.config.from_file(config_yaml)
 suite=conf.workflow
 
-
-print(repr(suite.Rocoto.scheduler))
+expname=conf.options.experiment_name
+logger.info(f'Experiment name: {expname}')
 
 rocoto_xml=crow.metascheduler.to_rocoto(suite)
-
-print(rocoto_xml)
+rocoto_xml_file=os.path.join(run_dir,f'{expname}.xml')
+logger.info(f'Rocoto XML file: {rocoto_xml_file}')
+with open(rocoto_xml_file,'wt') as fd:
+    fd.write(rocoto_xml)
+logger.info('Workflow XML file is generated.')
+logger.info('Use Rocoto to execute this workflow.')
