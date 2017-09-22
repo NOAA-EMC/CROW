5b6d9902d6d198b8544df1bdea435571628b48ba
samuel.trahan <Samuel.Trahan@noaa.gov>
2017-09-22 20:09:18 +0000

    models/fv3gfs now generates its rocoto xml

--- model/fv3gfs/setup_expt.py
+++ model/fv3gfs/setup_expt.py
@@ -20,21 +20,17 @@ for key in list(conf.keys()):
     if isinstance(conf[key],Platform) and key!='platform':
         del conf[key]
 
-run_dir=conf.options.run_dir
-logger.info(f'Run directory: {run_dir}')
-config_yaml=os.path.join(run_dir,'config.yaml')
+EXPDIR=conf.options.EXPDIR
+logger.info(f'Run directory: {EXPDIR}')
+config_yaml=os.path.join(EXPDIR,'config.yaml')
 yaml=crow.config.to_yaml(conf)
 
-assert('namelist' in conf.generic_fcst)
-assert('namelist' in conf.fcst)
-assert('namelist' in conf.ens_fcst)
-
 try:
-    os.makedirs(run_dir)
+    os.makedirs(EXPDIR)
 except FileExistsError:
-    logger.warning(f'{run_dir}: exists')
+    logger.warning(f'{EXPDIR}: exists')
     if not force:
-        logger.error(f'{run_dir}: already exists.  Delete or use --force.')
+        logger.error(f'{EXPDIR}: already exists.  Delete or use --force.')
         sys.exit(1)
     logger.warning(f'--force given; will replace config.yaml without '
                    'deleting directory')
@@ -49,7 +45,7 @@ expname=conf.options.experiment_name
 logger.info(f'Experiment name: {expname}')
 
 rocoto_xml=crow.metascheduler.to_rocoto(suite)
-rocoto_xml_file=os.path.join(run_dir,f'{expname}.xml')
+rocoto_xml_file=os.path.join(EXPDIR,f'{expname}.xml')
 logger.info(f'Rocoto XML file: {rocoto_xml_file}')
 with open(rocoto_xml_file,'wt') as fd:
     fd.write(rocoto_xml)
