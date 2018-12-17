9c91ef37847da5134ed254c0bc08e95c27d86415
Jian Kuang <jian.kuang@noaa.gov>
2018-12-17 22:13:11 +0000

    newly added files to CROW repo

---
+++ setup_case.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -ue
+
+# Get the directory in which this script resides.  We'll assume the
+# yaml files are there:
+here=$( dirname "$0" )
+
+#if [[ ! -s .in-the-ecfutils-dir ]] ; then
+#    echo "This script must be within the ecf/ecfutils directory when running it." 1>&2
+#    exit 2
+#fi
+
+export WORKTOOLS_VERBOSE=NO
+export CROW_CONFIG=$( cd ../ )
+export crowdir=$( cd $here ; pwd -P )
+
+# Make sure this directory is in the python path so we find worktools.py:
+export PYTHONPATH=$here:$crowdir:${PYTHONPATH:+:$PYTHONPATH}
+
+source "$here/worktools.sh.inc"
+
+find_python36
+
+$python36 -c '
+import sys;
+import worktools;
+worktools.setup_case(sys.argv[1:])' \
+     "$@"
