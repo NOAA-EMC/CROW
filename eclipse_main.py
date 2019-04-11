89d89a09e3c8956e8bbfdb954ed59f17e0c9481b
Jian.Kuang <jian.kuang@noaa.gov>
2019-04-10 23:25:25 -0400

    minor bug fix

--- eclipse_main.py
+++ eclipse_main.py
@@ -12,7 +12,7 @@ import worktools;
 
 if __name__ == '__main__':
 #    print("Hello world CROW!")   
-    option1 = '-f'
-    casename = 'fv3q2fy19retro5-GFS@C768_ENKF@384+80MEM.yaml'
+    option1 = '-sf'
+    casename = 'tutorial_case'
     username = 'casetest1'
     worktools.setup_case([option1,casename,username])
