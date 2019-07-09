a6e2551e55c0051a937df7643ce5450bfe5b68d7
Jian.Kuang <jian.kuang@noaa.gov>
2019-07-09 14:56:38 +0000

    change for rocoto file generation in slurm system

--- crow/sysenv/schedulers/Slurm.py
+++ crow/sysenv/schedulers/Slurm.py
@@ -134,14 +134,13 @@ class Scheduler(BaseScheduler):
         space=self.indent_text
         sio=StringIO()
         if 'queue' in spec:
-            sio.write(f'{indent*space}<queue>{spec["queue"]!s}</queue>\n')
+            sio.write(f'{indent*space}<queue>{"batch"}</queue>\n')
+        if 'partition' in spec:
+            sio.write(f'{indent*space}<partition>{spec["queue"]!s}</partition>\n')
         if 'account' in spec:
             sio.write(f'{indent*space}<account>{spec["account"]!s}</account>\n')
         if 'project' in spec:
             sio.write(f'{indent*space}<account>{spec["project"]!s}</account>\n')
-        if 'partition' in spec and spec['partition']:
-            sio.write(f'{indent*space}<native>-l partition='
-                      f'{spec["partition"]!s}</native>\n')
         if 'account' in spec:
             sio.write(f'{indent*space}<account>{spec["account"]!s}</account>\n')
         if 'jobname' in spec:
