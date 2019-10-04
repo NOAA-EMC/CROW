9830c6897ec6e3fa865a8b755574c887b3891217
samuel.trahan <Samuel.Trahan@noaa.gov>
2019-10-04 20:52:07 +0000

    use !icalc in the imcalc test

--- examples/imcalc/imcalc.py
+++ examples/imcalc/imcalc.py
@@ -8,7 +8,11 @@ sys.path.append(os.getcwd() + '/../../')
 
 import logging
 from datetime import timedelta
-from crow.config import from_dir, Suite, from_file, to_yaml, evaluate_immediates
+from crow.config import from_dir, Suite, from_file, to_yaml, evaluate_immediates, from_string
+import crow.config.represent
+
+from crow.config.eval_tools import list_eval, dict_eval
+from crow.sysenv import JobResourceSpec
 
 platdoc=from_file('_common.yaml','_sandbox.yaml')
 platdoc.platform.Evaluate=True
@@ -18,40 +22,9 @@ shutil.copyfile('resources_sum_sample.yaml','resources_sum.yaml')
 doc=from_file('_common.yaml','_sandbox.yaml','case.yaml','default_resources.yaml','resources_sum.yaml')
 
 filename = 'resources_sum.yaml'
-rc_config = doc['partition_common']['resources']
 
-def to_py(o,memo=None):
-    if memo is None: memo=dict()
-    i=id(o)
-    if i in memo: return memo[i]
-    if isinstance(o,bytes) or isinstance(o,str):
-        ret=copy.copy(o)
-        memo[i]=ret
-    elif isinstance(o,collections.abc.Mapping):
-        ret=dict()
-        memo[i]=ret
-        for k,v in o.items():
-            py_k=to_py(k,memo)
-            py_v=to_py(v,memo)
-            ret[py_k]=py_v
-    elif isinstance(o,collections.abc.Sequence):
-        ret=list()
-        memo[i]=ret
-        for v in o:
-            py_v=to_py(v,memo)
-            ret.extend(py_v)
-    else:
-        ret=copy.copy(o)
-        memo[i]=ret
-    return ret
-
-py_config=to_py(rc_config)
-content = to_yaml(py_config)
-with open(filename,'wt') as fd:
-     fd.write(content)
-resource_sum = from_file(filename)
-value = { 'resources_sum': resource_sum }
-content = to_yaml(value)
+doc.writeme = { 'resources_sum': doc.partition_common.resources }
+content = to_yaml({ 'resources_sum': doc.partition_common.resources })
 with open(filename,'wt') as fd:
      fd.write(content)
 
